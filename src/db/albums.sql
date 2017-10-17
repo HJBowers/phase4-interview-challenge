3b61b3ade93c91e9510c190c72905b2c57b6a775
HJBowers <HJBowers@gmail.com>
2017-10-17 10:28:18 -0700

    updated schema to include reviews and users

--- src/db/albums.sql
+++ src/db/albums.sql
@@ -1,8 +1,26 @@
-INSERT INTO
-  albums (title, artist)
-VALUES
+INSERT INTO albums (title, artist) VALUES
   ('Malibu', 'Anderson .Paak'),
   ('A Seat at the Table', 'Solange Knowles'),
   ('Melodrama', 'Lorde'),
   ('In Rainbows', 'Radiohead')
+;
+
+INSERT INTO users(email, password, name) VALUES
+  ('foil@Al.com','a','Aluminum Foil'),
+  ('gold@Au.com','a','Gold Karats'),
+  ('iron@Fe.com','a','Tony Stark'),
+  ('silver@Ag.com','a','Norrin Radd')
+;
+
+INSERT INTO reviews(description, user_id, album_id) VALUES
+  ('London, the capital of England and the United Kingdom.','1','2'),
+  ('SF, the best city ever outside of Oakland.','1','2'),
+  ('New New York','2','4'),
+  ('The new frontier. Or something...','3','3'),
+  ('Oakland is soooo awesome!','4','1'),
+  ('So many less words','2','1'),
+  ('SF stufffff','4','2'),
+  ('New York New York New York New York','1','4'),
+  ('ROCKETS AND SHIPS IN SPACE!!','4','3'),
+  ('Meow meow meow','3','1')
 ;
--- src/db/schema.sql
+++ src/db/schema.sql
@@ -1,5 +1,26 @@
+DROP TABLE IF EXISTS albums;
 CREATE TABLE albums (
-  id SERIAL,
+  id SERIAL PRIMARY KEY,
   title VARCHAR(255) NOT NULL,
   artist VARCHAR(255) NOT NULL
+);
+
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL UNIQUE,
+  password VARCHAR(255) NOT NULL,
+  join_date TIMESTAMP DEFAULT current_timestamp
+);
+
+DROP TABLE IF EXISTS reviews;
+CREATE TABLE reviews(
+  id SERIAL PRIMARY KEY,
+  user_id INTEGER,
+  album_id INTEGER,
+  description TEXT NOT NULL,
+  date_created TIMESTAMP DEFAULT current_timestamp,
+  FOREIGN KEY (user_id) REFERENCES users(id),
+  FOREIGN KEY (album_id) REFERENCES albums(id)
 );
