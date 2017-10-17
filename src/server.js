4c91959ed8db6e96cbb05707b48c5f7254cbce2d
HJBowers <HJBowers@gmail.com>
2017-10-17 00:30:32 -0700

    update server file

--- src/server.js
+++ src/server.js
@@ -1,41 +1,32 @@
 const path = require('path')
 const express = require('express')
 const bodyParser = require('body-parser')
-const db = require('./db')
+const session = require('express-session')
+const routes = require('../src/routes/index')
 
 const port = process.env.PORT || 3000
 
 const app = express()
 
+app.use(session({
+  name: 'session',
+  secret: 'nom nom nom',
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    expires: 600000
+  }
+}))
+
 require('ejs')
+
 app.set('view engine', 'ejs')
-app.set('views', path.join(__dirname, 'views'))
+app.set('views', __dirname + '/views')
+// app.set('views', path.join(__dirname, 'views'))
 
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({extended: false}))
-
-app.get('/', (req, res) => {
-  db.getAlbums((error, albums) => {
-    if (error) {
-      res.status(500).render('error', {error})
-    } else {
-      res.render('index', {albums})
-    }
-  })
-})
-
-app.get('/albums/:albumID', (req, res) => {
-  const albumID = req.params.albumID
-
-  db.getAlbumsByID(albumID, (error, albums) => {
-    if (error) {
-      res.status(500).render('error', {error})
-    } else {
-      const album = albums[0]
-      res.render('album', {album})
-    }
-  })
-})
+app.use('/', routes)
 
 app.use((req, res) => {
   res.status(404).render('not_found')
